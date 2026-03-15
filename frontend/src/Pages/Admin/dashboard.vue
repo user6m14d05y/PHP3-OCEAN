57cfc1e8e243bef6484e658adec139070cc4c5bd
Bui Trong Thanh <buitrongthanh2k5@gmai.com>
2026-03-15 15:23:16 +0700

    update router fix home /, update html home cup header and footet  set role admin, user

--- frontend/src/Pages/Admin/dashboard.vue
+++ frontend/src/Pages/Admin/dashboard.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const isLoggedIn = ref(false);
+
+const logout = () => {
+    // Xóa session
+    sessionStorage.removeItem('user');
+    isLoggedIn.value = false;
+    
+    // Đẩy về Đăng nhập
+    router.push('/');
+};
 </script>
 
 <template>
@@ -9,6 +23,7 @@
         <div class="flex gap-4">
             <router-link to="/" class="px-4 py-2 bg-gray-500 text-white rounded shadow hover:bg-gray-600 transition">Về Trang chủ (Home)</router-link>
             <router-link to="/client" class="px-4 py-2 bg-green-500 text-white rounded shadow hover:bg-green-600 transition">Sang Trang Client</router-link>
+            <button @click="logout" class="px-4 py-2 bg-green-500 text-white rounded shadow hover:bg-green-600 transition">Đăng xuất</button>
         </div>
     </div>
 </template>
