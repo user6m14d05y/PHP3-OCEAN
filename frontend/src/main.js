cf02aefb8f9e527ae85b34e7bfba30dc5828ea66
Bui Trong Thanh <buitrongthanh2k5@gmai.com>
2026-03-15 15:23:16 +0700

    update router fix home /, update html home cup header and footet  set role admin, user

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,19 +3,24 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import './assets/main.css';
 
-// Import Pages
-import Home from './Pages/Home.vue';
+
+// CLIENT
+import Home from './Pages/Client/Home/Index.vue';
 import ClientHome from './Pages/Client/Home/Index.vue';
 import ClientLogin from './Pages/Client/Auth/Login.vue';
 import ClientRegister from './Pages/Client/Auth/Register.vue';
 import ClientForgot from './Pages/Client/Auth/Forgot.vue';
+
+// ADMIN
+import AdminDashboard from './Pages/Admin/dashboard.vue';
 
 const routes = [
   { path: '/', component: Home, name: 'home' },
   { path: '/client/home', component: ClientHome, name: 'client-home' },
   { path: '/client/login', component: ClientLogin, name: 'client-login' },
   { path: '/client/register', component: ClientRegister, name: 'client-register' },
-  { path: '/client/forgot', component: ClientForgot, name: 'client-forgot' }
+  { path: '/client/forgot', component: ClientForgot, name: 'client-forgot' },
+  { path: '/admin/dashboard', component: AdminDashboard, name: 'admin-dashboard' }
 ];
 
 const router = createRouter({
