dfd8f3ecbc5bbbe71f0b7e57036dfe7080a82426
Bui Trong Thanh <buitrongthanh2k5@gmai.com>
2026-03-23 20:30:06 +0700

    CRUD coupon nang cao admin giam tien mat co dinh, giam theo %, freeship, giam toi da, don toi thieu, gioi han kho, gioi han nguoi dung, co filter search, an hien, public ma giam gia, quan ly thoi gian, tang ma giam gia dau tien cho user, tim kiem

---
+++ backend/app/Models/UserCoupon.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserCoupon extends Model
+{
+    use HasFactory;
+
+    // Chỉ định rõ tên bảng (đề phòng Laravel không map đúng số nhiều)
+    protected $table = 'user_coupons';
+
+    // Cho phép Cập nhật, Thêm dữ liệu vào các cột này
+    protected $fillable = [
+        'user_id',
+        'coupon_id',
+        'used_count',
+        'is_saved'
+    ];
+
+    /**
+     * Lấy thông tin User đã lưu mã
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Lấy chi tiết thông tin Mã giảm giá
+     */
+    public function coupon()
+    {
+        return $this->belongsTo(Coupon::class);
+    }
+}
